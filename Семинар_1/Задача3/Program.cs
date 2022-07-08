41c94eb51f3a5fe9f12caae49a34462abb8baee7
Dmitriy <klimov.dima@mail.ru>
2022-07-08 21:40:52 +0300

    процедура для функции

--- Семинар_1/Задача3/Program.cs
+++ Семинар_1/Задача3/Program.cs
@@ -1,11 +1,16 @@
-﻿// функция воздведения в квадрат
-
+﻿// функция возведения в квадрат
+void Zadacha5()
+{
 int number = 5;
 int result = Power(number);
 Console.WriteLine(result);
-
+}
 
 int Power(int currentNumber)
 {
     return currentNumber * currentNumber;
 }
+
+
+
+Zadacha5()
