e7a43e62d0244c869d7cd808a5b8904844f06a84
Dmitriy <klimov.dima@mail.ru>
2022-07-08 21:30:30 +0300

    Создал функцию

--- Семинар_1/Задача3/Program.cs
+++ Семинар_1/Задача3/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿// функция воздведения в квадрат
+
+int number = 5;
+int result = Power(number);
+Console.WriteLine(result);
+
+
+int Power(int currentNumber)
+{
+    return currentNumber * currentNumber;
+}
