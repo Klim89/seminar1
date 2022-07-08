59ee8a090ee70ca4bc3dd5f17fb0ae2dcbcb9752
Dmitriy <klimov.dima@mail.ru>
2022-07-08 22:27:18 +0300

    Нахождение целого числа в промежутке

--- Семинар_1/Задача4/Program.cs
+++ Семинар_1/Задача4/Program.cs
@@ -1,1 +1,11 @@
 ﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+int number = 3;
+int start = -number;
+while (start < number)
+{
+    Console.Write(start + "  ");
+    start++;
+}
+Console.Write(number);
+
