8564f2254f1c0733bd0f1531ae096a983c783723
Dmitriy <klimov.dima@mail.ru>
2022-07-08 20:48:11 +0300

    Первая задача альтернативное решение

--- Семинар_1/Program.cs
+++ Семинар_1/Program.cs
@@ -1,9 +1,29 @@
-﻿int n1=5;
-int n2=25;
+﻿int n1=9;
+int n2=-3;
 
 if (n1==n2*n2)
+{
 Console.WriteLine ("Yes");
-
+}
 else
+{
 Console.WriteLine ("No");
+}
 
+
+Console.Write("Введите число 1: ");
+int number_1 = Convert.ToInt32 (Console.ReadLine());
+Console.Write("Введите число 2: ");
+int number_2 = Convert.ToInt32 (Console.ReadLine());
+
+
+if(number_2 == number_1 * number_1)
+
+{
+
+    Console.WriteLine("2 число является квадратом первого");
+}
+else
+{
+    Console.WriteLine("2 число не является квадратом первого");
+}
