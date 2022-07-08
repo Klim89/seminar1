5902f657b1d1cf0129965919e3ac27f14124eafd
Dmitriy <klimov.dima@mail.ru>
2022-07-08 21:07:59 +0300

    Вторая задача

---
+++ Семинар_1/Задача2/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Write("Введите число дня недели: ");
+int number_week = Convert.ToInt32(Console.ReadLine());
+
+if(number_week == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+ else if(number_week == 2)
+{
+    Console.WriteLine("Вторник");
+}
+else if(number_week == 3)
+{
+    Console.WriteLine("Среда");
+}
+else if(number_week == 4)
+{
+    Console.WriteLine("Четверг");
+}
+else if(number_week == 5)
+{
+    Console.WriteLine("Пятница");
+}
+else if(number_week == 6)
+{
+    Console.WriteLine("Суббота");
+}
+else if(number_week == 7)
+{
+    Console.WriteLine("Воскресение");
+}
+else
+{
+    Console.WriteLine("Указано некорректное число недели");
+
+}
