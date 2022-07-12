f373f8775956a292c6ec205d89b673f07004be38
Dmitriy <klimov.dima@mail.ru>
2022-07-12 21:07:17 +0300

    Вывод случайного 3-х значного числа

---
+++ Семинар_1/Задача6/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+//456 -> 46
+//782 -> 72
+//918 -> 98
+
+void Zadacha06()
+{
+    Random rand = new Random();
+
+    int number = rand.Next(100, 1000);
+    Console.WriteLine(number);
+    int ones = number % 10;
+    int tens = number / 10;
+    int hungs = number / 100;
+
+    int resultNumber = ones + hungs * 10;
+    Console.WriteLine(resultNumber);
+}
+
+Zadacha06();
