0bc434450bc6a5b48dd4c3e36ea9dcb52fa56018
Dmitriy <klimov.dima@mail.ru>
2022-07-12 21:30:55 +0300

    Проверка на кратность

---
+++ Семинар_1/Задача7/Program.cs
@@ -0,0 +1,22 @@
+﻿//  Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+
+void Zadacha07()
+{
+    Console.Write("Введите число 1: ");
+    int number1 = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите число 2: ");
+    int number2 = Convert.ToInt32(Console.ReadLine());
+    int div = number2 % number1;
+    if (div == 0)
+    {
+    Console.WriteLine(number2 + " - является кратным" + " " + number1);
+    }
+    else
+    {
+    Console.WriteLine(number2 + " - не является кратным" + " " + number1);
+    }   
+
+    Console.WriteLine(div);
+}
+
+Zadacha07();
