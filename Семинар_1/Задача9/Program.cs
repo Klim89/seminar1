6cc98ec1019cd64fa6a24e87f66dc8e11f027e1b
Dmitriy <klimov.dima@mail.ru>
2022-07-12 22:18:42 +0300

    Проверка на степень 2-х чисел

---
+++ Семинар_1/Задача9/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+
+void Zadacha09()
+{
+    Console.Write("Введите число 1: ");
+    int number1 = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите число 2: ");
+    int number2 = Convert.ToInt32(Console.ReadLine());
+    
+    if (number1 == number2*number2)
+        {
+            Console.WriteLine(number1 + " - является квадратом" + " " + " "+ number2);
+        }
+    else if (number2 == number1*number1)
+        {
+        Console.WriteLine(number2 + " - является квадратом" + " " + " "+ number1);
+        }
+    else 
+        {
+            Console.WriteLine(number1 + " - не является квадратом"  + " "+ number2);
+            Console.WriteLine(number2 + " - не является квадратом"  + " "+ number1);
+        }   
+    }
+
+
+Zadacha09();
