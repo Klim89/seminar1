c8e27c8a1f8aa5c1f2027fbef865819e2c17d21c
Dmitriy <klimov.dima@mail.ru>
2022-08-02 20:39:04 +0300

    Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А

---
+++ Семинар_1/Задача14/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+void Zadacha14()
+{
+    Console.WriteLine("Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А. ");
+    Console.WriteLine("Введите число А: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    int sum = 0;
+
+    for (int i = 0; i <= number; i++)
+    {
+        sum += i;
+    }
+
+    Console.Write($"Сумма чисел от 1 до {number} равна {sum}");
+        
+    
+}
+Zadacha14();
